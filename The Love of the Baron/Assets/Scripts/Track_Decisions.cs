ed2ea96a416ac8e54cc60fe0aca21ca9b8ff7842
Boluh <chris.beil821@gmail.com>
2022-02-26 15:29:39 +0100

    Hexagon automatisch zeichnen lassen und text scroll

--- The Love of the Baron/Assets/Scripts/Track_Decisions.cs
+++ The Love of the Baron/Assets/Scripts/Track_Decisions.cs
@@ -5,30 +5,69 @@ using UnityEngine;
 public class Track_Decisions : MonoBehaviour
 {
     
-    public Vector2 liebe;
-    public Vector2 hass;
-    public Vector2 gier;
-    public Vector2 neid;
-    public Vector2 pazifismus;
-    public Vector2 wissen;
-    
+    private Vector3 liebe;
+    private Vector3 hass;
+    private Vector3 gier;
+    private Vector3 neid;
+    private Vector3 pazifismus;
+    private Vector3 wissen;
+
+
+    public LineRenderer drawHexagon;
+
+
+public Vector3 startLiebe;
+public Vector3 endLiebe;
+public Vector3 startHass;
+public Vector3 endHass;
+public Vector3 startGier;
+public Vector3 endGier;
+public Vector3 startNeid;
+public Vector3 endNeid;
+public Vector3 startPazifismus;
+public Vector3 endPazifismus;
+public Vector3 startWissen;
+public Vector3 endWissen;
+    private void Start() {
+    liebe = startLiebe;
+    hass = startHass;
+    gier = startGier;
+    neid =  startNeid;
+    pazifismus = startPazifismus;
+    wissen = startWissen;
+    }
 
 public void AddLiebe(float Amount){
- liebe = liebe + new Vector2 (0,Amount);
+ 
+ liebe = Vector3.MoveTowards(liebe,endLiebe,Amount);
+
  }
 public void AddHass(float Amount){
- hass = hass + new Vector2 (0,Amount);
+ hass = Vector3.MoveTowards(hass,endHass,Amount);
  }
  public void AddGier(float Amount){
- gier = gier + new Vector2 (0,Amount);
+ gier = Vector3.MoveTowards(gier,endGier,Amount);
+
  }
  public void AddNeid(float Amount){
- neid = neid + new Vector2 (0,Amount);
+ neid = Vector3.MoveTowards(neid,endNeid,Amount);
  }
  public void AddPazifismus(float Amount){
- pazifismus = pazifismus + new Vector2 (0,Amount);
+ pazifismus = Vector3.MoveTowards(pazifismus,endPazifismus,Amount);
  }
  public void AddWissen(float Amount){
- wissen = wissen + new Vector2 (0,Amount);
+ wissen = Vector3.MoveTowards(wissen,endWissen,Amount);
  }
+
+private void Update() {
+drawHexagon.SetPosition(0,liebe);
+drawHexagon.SetPosition(1,hass);
+drawHexagon.SetPosition(2,gier);
+drawHexagon.SetPosition(3,neid);
+drawHexagon.SetPosition(4,pazifismus);    
+drawHexagon.SetPosition(5,wissen);
+
+
+}
+
 }
