cebdeaab95b9f64955e43154e0a93944b5585a1e
TheGreatShaka <sargon2222@outlook.com>
2022-02-28 14:27:47 +0100

    New functionality to Options2

--- The Love of the Baron/Assets/Scripts/ResolutionManager.cs
+++ The Love of the Baron/Assets/Scripts/ResolutionManager.cs
@@ -17,13 +17,30 @@ public class ResolutionManager : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
         }
 
         resolutionDropdown.AddOptions(options);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
     public void SetFullscreen(bool isFullscreen)
     {
